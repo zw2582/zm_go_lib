30c6caed20121f863b5e68702b4a7c85bbe537d3
zhouwei <zhouwei@shzhanmeng.com>
2019-06-26 18:28:59 +0800

    'f'

--- controllers/base.go
+++ controllers/base.go
@@ -1,16 +1,19 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"strings"
+)
 
 type BaseController struct {
 	beego.Controller
 }
 
 //返回成功
-func (c *BaseController) AjaxSucc(data interface{}, msg string) {
+func (c *BaseController) AjaxSucc(data interface{}, msg ...string) {
 	c.Data[`json`] = map[string]interface{} {
 		`status`:0,
-		`msg`:msg,
+		`msg`:strings.Join(msg, ","),
 		`data`:data,
 	}
 	//beego.Debug(fmt.Sprintf("ajaxSucc:%+v", c.Data[`json`]))
@@ -18,10 +21,10 @@ func (c *BaseController) AjaxSucc(data interface{}, msg string) {
 }
 
 //返回失败
-func (c *BaseController) AjaxFail(data interface{}, msg string) {
+func (c *BaseController) AjaxFail(data interface{}, msg ...string) {
 	c.Data[`json`] = map[string]interface{} {
 		`status`:1,
-		`msg`:msg,
+		`msg`:strings.Join(msg, ","),
 		`data`:data,
 	}
 	//beego.Debug(fmt.Sprintf("ajaxSucc:%+v", c.Data[`json`]))
